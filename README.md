b24ed71693c88fbeb17b2aab4aae47d375e4fc5c
Gerardo Valenzuela <ilovesciencefb@gmail.com>
2018-04-25 13:11:20 -0700

    the first push for the ui

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the source for the UI side of my site.
---
+++ pages/privacy.js
@@ -0,0 +1,9 @@
+import '../styles/index.scss';
+
+const Privacy = () => (
+    <div className='blue-text'>
+       Your privacy matters. Learn more.
+    </div>
+);
+
+export default Privacy;
