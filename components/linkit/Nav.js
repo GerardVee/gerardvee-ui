92dfb4758037e948f0affe1e74dd419bce17d6bf
Gerardo Valenzuela <ilovesciencefb@gmail.com>
2018-05-04 20:57:17 -0700

    second try, this is annoying

--- components/linkit/Nav.js
+++ components/linkit/Nav.js
@@ -4,11 +4,9 @@ export default ({ picture, children }) => (
             <h1 className='title'><span className='colored'>L</span>ink<span className='colored'>I</span>t</h1>
             <h2 className='subtitle'>Relevant linking made easy. No sub categories, no comments, no displayed stats, just links. No account required to post.</h2>
         </div>
-        <div class="user-options">
-            { picture && <img src={ picture } height='50' width='50' /> }
-            <div className='options'>
-                { children }
-            </div>
+        { picture && <img src={ picture } height='50' width='50' /> }
+        <div className='options'>
+            { children }
         </div>
     </div>
 );
