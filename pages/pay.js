f0223bbcb19b50c6bfa217d9240d10da13528b7c
Gerardo Valenzuela <ilovesciencefb@gmail.com>
2018-05-12 02:12:05 -0700

    tests are working well

--- pages/pay.js
+++ pages/pay.js
@@ -7,12 +7,13 @@ import '../styles/index.scss';
 
 const api = 'https://api.gerardvee.com/';
 
-const onToken = async (token, href) =>
+const onToken = async ({ id }, amount, href) =>
 {
-    const res = await fetch(api + 'pay', post(token));
+    const res = await fetch(api + 'charge', post({ token: id, amount }));
     const data = await res.json();
-    alert(`Thank you, ${ data.email }`);
-    window.location.href = decodeURIComponent(href) + `?token=${ data }`
+    console.log(data);
+    alert(`Thank you, $${ data.amount / 100 } charged.`);
+    window.location.href = decodeURIComponent(href) + `?token=${ data.id }`
 };
 
 export default class Pay extends React.Component
@@ -24,6 +25,9 @@ export default class Pay extends React.Component
         const { ref, amount } = query;
         return { href: ref, amount };
     }
+
+// if something is 25, add 2.6% then add .3 onto it,
+// due to the x - 2.6% - .3 net income rule 
 
     render()
     {
@@ -39,7 +43,7 @@ export default class Pay extends React.Component
                     name='gerardvee.com'
                     image='// gv logo'
                     amount={ amount * 100 } // in cents
-                    token={ (tkn) => onToken(tkn, href) } stripeKey='pk_test_fwQdo6ZSLVSW9YIhSGndRtkZ'>
+                    token={ (tkn) => onToken(tkn, amount * 100, href) } stripeKey='pk_test_fwQdo6ZSLVSW9YIhSGndRtkZ'>
                     <button>Pay ${ amount }</button>
                 </StripeCheckout>
                 }
@@ -50,7 +54,7 @@ export default class Pay extends React.Component
                             name='gerardvee.com'
                             image='// gv logo'
                             amount={ payment * 100 } // in cents
-                            token={ (tkn) => onToken(tkn, href) } stripeKey='pk_test_fwQdo6ZSLVSW9YIhSGndRtkZ'>
+                            token={ (tkn) => onToken(tkn, payment * 100, href) } stripeKey='pk_test_fwQdo6ZSLVSW9YIhSGndRtkZ'>
                             <button>Donate</button>
                         </StripeCheckout>
                     </div>
