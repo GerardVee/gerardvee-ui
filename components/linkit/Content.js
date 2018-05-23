d27f3e3ecbf3b3508407f79a7b986f6eed9241e2
Gerardo Valenzuela <ilovesciencefb@gmail.com>
2018-05-22 18:10:06 -0700

    design updates

--- components/linkit/Content.js
+++ components/linkit/Content.js
@@ -1,3 +1,7 @@
+import React from 'react';
+
+const api = 'https://api.gerardvee.com/';
+
 const Option = ({ children, icon }) => (
     <div className='row valign option'>
         <i className='material-icons'>{ icon }</i>
@@ -13,9 +17,47 @@ const ProfilePic = ({ url, ifEmpty, shape, profile }) => (
     <img style={{ borderRadius: '50%', height: 40, width: 40 }} src={ !!profile ? url : ifEmpty } />
 );
 
-const Posts = () => (
-    <div></div>
-);
+const withPosts = (Component) =>
+    class extends React.Component
+    {
+        state = { posts: [] };
+        async componentWillMount()
+        {
+            const res = await fetch(api + 'linkit/posts');
+            const posts = await res.json();
+            this.setState({ posts });
+        }
+        render()
+        {
+            const { posts } = this.state;
+            return <Component posts={ posts } />;
+        }
+    };
+
+const Posts = withPosts(({ posts }) => (
+    <div className='col'>
+    { posts.map(post => (
+        <div className='col halign-children' style={{ border: 'solid 0.1em black', borderRadius: '1em', marginBottom: '0.3em' }}>
+            <a className='center' href={ post.link }><h1 style={{ fontSize: '2.7em' }}>{ post.title }</h1></a>
+            <div className='row halign-children'>
+                <div className='row valign flex-1'>
+                    <i className='material-icons'>keyboard_arrow_up</i>
+                    Upvote
+                </div>
+                <div className='row valign flex-1'>
+                    <i className='material-icons'>keyboard_arrow_down</i>
+                    Downvote
+                </div>
+                <div className='row valign flex-1'>
+                    <i className='material-icons'>error_outline</i>
+                    Report
+                </div>
+            </div>
+        </div>
+    ))}
+    { JSON.stringify(posts) }
+    </div>
+));
 
 export default ({ profile }) => (
     <main>
