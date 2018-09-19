08d8717bb43d0428d9bf1d0ea58a2cc4095e6add
Gee Vee <ilovesciencefb@gmail.com>
2018-09-19 01:37:45 -0700

    social links

--- components/site/Footer.js
+++ components/site/Footer.js
@@ -2,9 +2,9 @@ export default () => (
     <div className='col'>
         <h3 className='center site-footer-title'>Keep in touch</h3>
         <div className='row halign'>
-            <button className='site-footer-github-button'>Github</button>
-            <button className='site-footer-linkedin-button'>LinkedIn</button>
-            <button className='site-footer-codepen-button'>CodePen</button>
+            <button className='site-footer-github-button' onClick={ () => window.location.href = 'https://github.com/GerardVee' }>Github</button>
+            <button className='site-footer-linkedin-button' onClick={ () => window.location.href = 'https://www.linkedin.com/in/gerardo-valenzuela-336496116/' }>LinkedIn</button>
+            <button className='site-footer-codepen-button' onClick={ () => window.location.href = 'https://codepen.io/geevee/' }>CodePen</button>
         </div>
         <p className='center site-footer-copyright'>Crafted by me ©, 2017 </p>
     </div>
