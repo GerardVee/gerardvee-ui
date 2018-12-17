554d6573d39d576994d039cd3722eae3dd19eaba
Gee Vee <ilovesciencefb@gmail.com>
2018-12-16 23:12:34 -0800

    404 page added

---
+++ pages/_error.js
@@ -0,0 +1,12 @@
+import '../styles/contact.scss';
+import Page from '../components/Page';
+import React from 'react';
+
+export default () => (
+    <Page title='404'>
+        <div className='col halign center'>
+            <h3 className='site-contact-preface-title'>404</h3>
+            <h3 className='site-contact-preface-title'>Page Not Found :(</h3>
+        </div>
+    </Page>
+);
